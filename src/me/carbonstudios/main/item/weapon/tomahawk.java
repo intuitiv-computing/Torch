d50780c72bf7adcbbe56b30a654e67fc029fef6b
sam4215 <sam4215@users.noreply.github.com>
2015-04-04 20:03:23 -0500

    Add a Tomahawk
    Added Tomahawk.java, extends Weapon

---
+++ src/me/carbonstudios/main/item/weapon/tomahawk.java
@@ -0,0 +1,21 @@
+package me.carbonstudios.main.item.weapon;
+
+import me.carbonstudios.main.*;
+
+public class Tomahawk extends Weapon {
+    public int id() {
+        return 3;
+    }
+    public int damage() {
+        return 3;
+    }
+    public List<String> bonus {
+        return null;
+    }
+    public String name() {
+        return "Tomahawk";
+    }
+    public int type() {
+        return 1;
+    }
+}
