809696135b31e8332947708ae391dfe4bb829b6e
sam4215 <sam4215mc@gmail.com>
2015-04-25 20:23:36 -0500

    No peek

---
+++ src/me/carbonstudios/main/EasterEgg.java
@@ -0,0 +1,14 @@
+package me.carbonstudios.main;
+
+/**
+ * THIS CODE MAY BE LICENSED AND/OR COPYRIGHTED BY CARBON STUDIOS.
+ * <p/>
+ * FILE CREATED ON 4/25/2015 at 8:07 PM
+ */
+public class EasterEgg {
+    String easterEggs = "C: !vuun hpuW\"            \n" +
+            "+ \"v\\\"            \n" +
+            "+ \"HE%TO$ hcur ,}RVH18#3%{ vu cvkug ho vtr phinvt hml ,vuun avewur cxheg 'cpuinnts' lhprhn t ne hphmL\"            \n" +
+            "+ \"v\\\"            \n" +
+            "+ \"lhv.nhwtadlejelthpr.hcurvh .dtidvt chnphjhp ne lfhl hml cvt 'hwtamrpul' ne dhy hml ,hrvhphghp puG .lfhl chlsdprvh ne nemL";
+}
